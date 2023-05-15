c9f719b118903544cf3980ce3494878e15dedb62
cloudsony999 <cloudsony999@gmail.com>
2023-05-15 14:07:08 +0530

    first commit

---
+++ oops.py
@@ -0,0 +1,13 @@
+class Student:
+    def call(self):
+        print('I am a student from the class!!!!....')
+
+s=Student()
+
+s.call()#OOPS
+
+def call():
+    print('I am a procudure....')
+
+call()#Procedural
+
---
+++ test.py
@@ -0,0 +1,3 @@
+import keyword
+x=keyword.iskeyword("true")
+print(' if is a keyword ',x)
